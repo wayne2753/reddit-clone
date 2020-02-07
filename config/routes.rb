384905a749a17d6c936a72307ce98d0e751dd3b7
簡維德 <aeiou501@gmail.com>
2020-02-07 19:53:30 +0800

    add profile page, create subscribers table

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :accounts
 
-  get "u/:username" => "profile#index", as: :profile
+  get "u/:username" => "public#profile", as: :profile
   resources :communities do
     resources :posts
   end
+  
   root to: "public#index"
 end
