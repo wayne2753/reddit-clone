6beef7b9030b04b97c615de95a48776b84d0974d
簡維德 <aeiou501@gmail.com>
2020-02-12 16:04:22 +0800

    fix text error in comment model, add comment form in post/show page

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,6 @@ class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :account
 
-  validate_presence_of :message, :account_id, :post_id
+  validates_presence_of :message, :account_id, :post_id
   
 end
