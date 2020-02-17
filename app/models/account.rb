577313e2218a6c26c132b7069fc9f4ed5349f906
gwq <1305851004@qq.com>
2020-02-17 10:30:48 +0800

    fixed ActiveRecord::HasManyThroughAssociationNotFoundError

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,6 +3,7 @@ class Account < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :subscriptions
   has_many :communities, through: :subscriptions
   has_many :posts
   has_many :comments
