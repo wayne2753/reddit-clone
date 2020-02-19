b438df7d714b315aedd7dc872f6e4b28a5f510d4
簡維德 <aeiou501@gmail.com>
2020-02-19 16:06:47 +0800

    add jquery, vote model controller and arrow btn

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 
 require("bootstrap")
+require("../includes/vote.js")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
